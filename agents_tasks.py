b2378acf439391337371ede661ee460a60422b66
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-09-25 22:50:41 +0530

    Update agents_tasks.py

--- agents_tasks.py
+++ agents_tasks.py
@@ -1,9 +1,17 @@
 from crewai import Agent, Task, Crew, Process
+from crewai.tasks import TaskOutput 
 from crewai_tools import WebsiteSearchTool, ScrapeWebsiteTool
 from custom_tools import fetch_stock_data, fetch_stock_news
 
 search_tool = WebsiteSearchTool()
 scrape_tool = ScrapeWebsiteTool()
+
+# Callbacks 
+# def crew_step_callback(agent, task, step):
+#     print(f"Step Callback - Agent: {agent.role}, Task: {task.description}, Step: {step}")
+
+# def crew_task_callback(output: TaskOutput):
+#     print(f"Task Callback - Task Completed: {output.description}")
 
 # Data collection 
 data_collector = Agent(
@@ -17,7 +25,7 @@ data_collector = Agent(
 )
 
 data_collection_task = Task(
-    description="Collect key stock data metrics for {company_stock} using its ticker format.",
+    description="Collect key stock data metrics for {company_stock} using its ticker format. Use only the data provided and do not request for more metrics.",
     expected_output="Data about most relevant financial metrics for stock analysis.",
     agent=data_collector,
     async_execution=False,
@@ -48,16 +56,22 @@ stock_market_researcher = Agent(
     backstory=("An experienced stock market researcher who gathers information from sources to offer insights about the performance of the company."),
     tools=[search_tool, scrape_tool],  
     verbose=True,
-    max_iter=5,
+    max_iter=10,
     allow_delegation=False,  
+    max_execution_time=180,
+    max_retry_limit=2,
 )
 
-stock_market_research_task = Task( # Separate into multiple tasks for the same agent 
+stock_market_research_task = Task( 
     description=(
-        "Conduct research on {company_stock} focusing on:\n"
+        "Conduct research on {company_stock} focusing on these topics:\n"
         "- General market trends effecting the company.\n"
         "- Industry comparisons between competitors and recent events affecting the company.\n"
         "- Risks and opportunities related to current market conditions.\n"
+        "Rules: Perform up to 3 different search queries per topic. "
+        "Do not repeat the same queries if the result is not helpful. "
+        "If the information is unavailable after the 3 attempts then skip to the next topic. "
+        "IMPORTANT: If the website returns a cookie consent information prevents scraping content after 1 attempt then never search that website again."
     ),
     expected_output=(
         "A clear analysis of {company_stock} covering market trends, industry comparisons, risks and opportunities."
@@ -96,11 +110,12 @@ manager = Agent(
     backstory=(
         "An expert project manager with a deep understanding of financial analysis and stock market trends. "
         "You are responsible for managing the flow of tasks, ensuring data collection, research and report writing are done in a coordinated manner."
-        "Do not repeat the task that handles stock data collection and use only the data that is provided."
+        "You must make sure that no tasks are repeated and only use the information provided."
     ),
     allow_delegation=True,  
 )
 
+# Hierarchical Agents
 crew = Crew(
     agents=[data_collector, news_reader, stock_market_researcher, financial_analyst],
     tasks=[data_collection_task, news_reader_task, stock_market_research_task, financial_analysis_task],
@@ -109,4 +124,17 @@ crew = Crew(
     full_output=True,
     verbose=True, 
     memory=True, 
+    # planning=True,
+    # step_callback=crew_step_callback,
+    # task_callback=crew_task_callback,
 )
+
+# Sequential Agents
+# crew = Crew(
+#     agents=[data_collector, news_reader, financial_analyst],
+#     tasks=[data_collection_task, news_reader_task, financial_analysis_task],
+#     process=Process.sequential,  
+#     full_output=True,
+#     verbose=True, 
+#     memory=True, 
+# )
